06da5390289368f94bfc9d8b1b2dd9bcd3475cd2
aitor <aitorman@outlook.com>
2022-05-26 18:29:37 +0200

    version final v1

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -2,11 +2,19 @@ import org.junit.Test;
 import org.junit.Ignore;
 import static org.junit.Assert.*;
 
+/* Clase calculadoraTest
+
+No tiene argumentos.
+Contiene un main para ejecutar una serie de batería de test a la clase Calculadora
+Implementa series de 3 test para cada uno de los métodos suma, resta, multiplicacion y raiz de la clase calculadora
+
+*/
 public class CalculadoraTest {
 
     private static final double DELTA = 1e-2;
 
     public static void main(String args[]) {
+
 
         testSuma();
         testResta();
@@ -21,6 +29,8 @@ public class CalculadoraTest {
         double[][] cases = { { 1.0, 3.4, 4.4 }, { 3.8, 4, 7.8 }, { 3, 0, 3 } };
         double resultado;
         double suma;
+        /* itera sobre los casos probando que los argumentos de cada uno de los tres test se corresponde con el 
+        resultado esperado */
         for (int i = 0; i < cases.length; i++) {
             // arrange
             resultado = cases[i][2];
@@ -29,7 +39,8 @@ public class CalculadoraTest {
             // assert
             //assert suma == resultado : "Error en la suma"; otra opcion con assert
             //assertEquals(resultado, suma, DELTA); //otra opcion con assert
-           
+            /* si el resultado del metodo suma se corresponde con el pasado por el array de los test
+            el test se da por bueno, de lo contrario determina fallo */
             if (suma == resultado) {
                 System.out.println("Test suma OK en caso " + i);
             } else {
@@ -51,9 +62,12 @@ public class CalculadoraTest {
             resta = calc.resta(cases[i][0], cases[i][1]);
             // assert
             //segundo test da falso negativo por redondeo 
-            System.out.println(resta);
-            System.out.println(resultado);
+            //System.out.println(resta);
+            //System.out.println(resultado);
             //assertEquals(resultado, resta, DELTA); // PUEDE SER UNA OPCION??
+            /* si el resultado del metodo resta se corresponde con el pasado por el array de los test
+            el test se da por bueno, de lo contrario determina fallo.
+            Se acepta un error menor de 10e-3*/
             if (Math.abs(resta-resultado)<0.001) { 
                 System.out.println("Test resta OK en caso " + i);
             } else {
@@ -74,6 +88,8 @@ public class CalculadoraTest {
             // act
             multiplicacion = calc.multiplica(cases[i][0], cases[i][1]);
             // assert
+            /* si el resultado del metodo multiplicacion se corresponde con el pasado por el array de los test
+            el test se da por bueno, de lo contrario determina fallo */
             if (multiplicacion == resultado) {
                 System.out.println("Test multiplicacion OK en el caso " + i);
             } else {
@@ -97,6 +113,9 @@ public class CalculadoraTest {
             // error de falso negativo en el primer caso por redondeo
             //System.out.println(division);
             //System.out.println(resultado);
+            /* si el resultado del metodo division se corresponde con el pasado por el array de los test
+            el test se da por bueno, de lo contrario determina fallo.
+            Se acepta un error menor de 10e-3*/
             if (Math.abs(division-resultado)<0.001) {
                 System.out.println("Test division OK en el caso " + i);
             } else {
@@ -120,6 +139,10 @@ public class CalculadoraTest {
             // error falso negativo en primero y segundo caso por redondeo
             //System.out.println(raiz);
             //System.out.println(resultado);
+
+            /* si el resultado del metodo raiz se corresponde con el pasado por el array de los test
+            el test se da por bueno, de lo contrario determina fallo.
+            Se acepta un error menor de 10e-3*/
             if (Math.abs(raiz-resultado)<0.001) {
                 System.out.println("Test raiz OK en el caso " + i);
             } else {
