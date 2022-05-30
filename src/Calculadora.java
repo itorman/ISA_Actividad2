c570d49582bc7ba16b85fcd7d411bec602e1583c
aitor <aitorman@outlook.com>
2022-05-30 22:50:52 +0200

    recuperando branch junit

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,5 @@
-public class Calculadora{
+public class Calculadora {
+    
     public double suma(double a, double b){
         return a+b;
     }
@@ -11,7 +12,33 @@ public class Calculadora{
     public double division(double a, double b){
         return a/b;
     }
-    public double raiz(double a){
-        return Math.sqrt(a);
+    public double raiz(Double X) {
+        for (double i = 1; i < X; ++i) {
+            double p = i * i;
+            if (p == X) {
+                // perfect square
+                return i;
+            }
+            if (p > X) {
+                // found left part of decimal
+                return raiz(X, i - 1, i);
+            }
+        }
+        return Double.NaN;
+    }    
+    private double raiz(double X, double low, double high) {
+        double mid = (low + high) / 2;
+        double p = mid * mid;
+        // uncomment below line to see how we reach the final answer
+        // System.out.println(low + " " + high + " " + mid + " " + p);
+    
+        if (p == X || (Math.abs(X - p) < 0.001)) {
+            return mid;
+        }
+        if (p < X) {
+            return raiz(X, mid, high);
+        }
+        return raiz(X, low, mid);
     }
+
 }
